41ec0a53a83d6f43614a886515c502850cd63767
Efe <48628713+Raader@users.noreply.github.com>
2021-09-02 14:39:53 +0300

    Add loading

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import ImageCanvas from "../components/image_canvas";
 import ImageInput from "../components/image_input";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
   const [image, setImage] = useState({ file: undefined, url: "" });
   const [predictions, setPredictions] = useState([]);
 
@@ -18,20 +19,24 @@ export default function Home() {
 
   return (
     <div className="layout">
-      <h1 style={{ margin: "0 2rem" }}>Image Detection</h1>
+      <h1>Image Detection</h1>
       <div className="header">
         <ImageInput
           onInput={(image) => {
+            if (loading) return;
             setImage(image);
             setPredictions([]);
-            detectObjects(image).then((data) =>
-              setPredictions(data.predictions)
-            );
+            setLoading(true);
+            detectObjects(image).then((data) => {
+              setPredictions(data.predictions);
+              setLoading(false);
+            });
           }}
         ></ImageInput>
       </div>
       <div
         style={{
+          position: "relative",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -39,9 +44,30 @@ export default function Home() {
       >
         <ImageCanvas
           className="detection-canvas"
+          style={{ filter: loading ? "blur(8px)" : "" }}
           image={image?.url}
           predictions={predictions}
         ></ImageCanvas>
+        {loading ? (
+          <div className="load-cont">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="loading-icon"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+              />
+            </svg>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
